9c28fb76033ba11386ca9226711b35d957dbf88c
Renan Cardoso <renancardoso0307@gmail.com>
2022-10-27 18:31:29 -0300

    feat: toastify imported

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 
 import { BrowserRouter, Route, } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 import { Header } from './components/Header'
 import MainRoutes from './routes'
 import './styles/global.css'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   return (
+    <>
    <BrowserRouter>
    <Header/>
     <MainRoutes/>
    </BrowserRouter>
+   <ToastContainer/>
+   </>
   )
 }
 
